6f544706244b0229a4fa97daa27290ceeb4eb428
Mårten Åsberg <marten.asberg@outlook.com>
2021-02-27 11:56:28 +0100

    Init npm and TypeSctipt
    Co-authored-by: carlwiede <carlwie@student.chalmers.se>

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+	entry: path.resolve(__dirname, "./src/index.ts"),
+	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader"
+			},
+		]
+	},
+	resolve: {
+		extensions: [ ".ts", ".js" ]
+	},
+	output: {
+		filename: "index.js",
+		path: path.resolve(__dirname, "bundle")
+	},
+};
