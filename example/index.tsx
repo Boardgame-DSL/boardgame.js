0206acc4a0ad65f7253f89c2bf5aa18c553df307
Mårten Åsberg <marten.asberg@outlook.com>
2021-03-13 22:00:21 +0100

    TicTacToe in example

--- example/index.tsx
+++ example/index.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { GameOverStatus, NotificationArea, StartButton, TurnDisplay } from "../components";
 import { HexDisplay } from "./HexDisplay";
+import { TicTacToeDisplay } from "./TicTacToeDisplay";
 import "../src";
 
 ReactDOM.render(
 	<React.StrictMode>
 		<div className="controls">
-			<StartButton/>
+			<StartButton gameName="TicTacToe"/>
 			<TurnDisplay/>
 		</div>
 		<GameOverStatus/>
-		<HexDisplay/>
+		<TicTacToeDisplay/>
 		<NotificationArea/>
 	</React.StrictMode>,
 	document.querySelector("app")
